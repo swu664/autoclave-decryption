794231c70227ab5c29e5f0dbf26ecd8172b7e64d
Scarlett Wu <swu664@uwo.ca>
2025-09-22 21:03:36 -0400

    add decrypt_vigenere() method and letter-value mapping

--- main.py
+++ main.py
@@ -16,5 +16,29 @@ def parse_ciphertext():
     except FileNotFoundError:
         print("File not found. Please check the path and try again.")
 
+letter_value_dict = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6,
+                     "h": 7, "i": 8, "j": 9, "k": 10, "l": 11, "m": 12,
+                     "n": 13, "o": 14, "p": 15, "q": 16, "r": 17, "s": 18,
+                     "t": 19, "u": 20, "v": 21, "w": 22, "x": 23, "y": 24,
+                     "z": 25}
+
+def decrypt_vigenere(ciphertext, key):
+    if len(key) != len(ciphertext):
+        raise ValueError("Key length must match ciphertext length.")
+    
+    plaintext = []
+
+    for c, k in zip(ciphertext, key):
+        if c.isalpha():
+            c_val = letter_value_dict[c.lower()]
+            k_val = letter_value_dict[k.lower()]
+            p_val = (c_val - k_val) % 26
+            p_char = chr(p_val + ord('a'))
+            plaintext.append(p_char)
+        else:
+            plaintext.append(c)
+
+    return plaintext
+
 if __name__ == "__main__":
     parse_ciphertext()
