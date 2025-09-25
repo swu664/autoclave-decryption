047e987f2c93116357d12c55eedfc633ce15dcc1
Scarlett Wu <swu664@uwo.ca>
2025-09-24 22:27:34 -0400

    extend parse_ciphertext to return word and letter index mappings

--- main.py
+++ main.py
@@ -24,18 +24,25 @@ def parse_ciphertext():
     try:
         with open(file_path, 'r') as f:
             content = f.read()
-            words = content.replace('.', '').split()
-            word_dict = {word: i for i, word in enumerate(words)}
-            plaintext = re.sub(r"[a-zA-Z]", '_', content)
-            print("Word Dictionary:")
-            for word, i in word_dict.items():
-                print(f"{word}: {i}")
-            print(f"\nCurrent Plaintext: \n{plaintext}")
+
+            #create mapping of indices to ciphertext words
+            words = content.replace("\n","<nl> ").split()
+            ciphertext_words = {i: word for i, word in enumerate(words)}
+            print("Ciphertext Words:")
+            for i, word in ciphertext_words.items():
+                print(f"{i}: {word}")
+            
+            #create mapping of indices to ciphertext letters
+            letters = re.findall(r'[a-zA-Z]', content)
+            ciphertext_letters = {i: [letter, 0] for i, letter in enumerate(letters)}
+
+            print(f"\n{content}")
+            print(f"\nCurrent Plaintext: \n{display_plaintext(ciphertext_words, ciphertext_letters)}")
 
     except FileNotFoundError:
         print("File not found. Please check the path and try again.")
 
-letter_value_dict = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6,
+    return content, ciphertext_words, ciphertext_letters
                      "h": 7, "i": 8, "j": 9, "k": 10, "l": 11, "m": 12,
                      "n": 13, "o": 14, "p": 15, "q": 16, "r": 17, "s": 18,
                      "t": 19, "u": 20, "v": 21, "w": 22, "x": 23, "y": 24,
